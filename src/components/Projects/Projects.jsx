8511ca9a7de0e05cfe8f0e8133187ef35b552069
Ali-Raza-Parhar <aliraza.uostd@gmail.com>
2021-05-05 12:40:14 +0500

    Responsive Pages

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Projects.css';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LaunchIcon from '@material-ui/icons/Launch';
+import Project from './Project';
 
 export default function Projects(){
   return(
@@ -11,110 +10,7 @@ export default function Projects(){
           <h2>Projects</h2>
           <hr className="projectHrLine"/>
           <p>Some of my featured projects, see more on my <a href="#">Github</a>.</p>
-        </div>
-        <div className="projectDetail">
-          <h3>Title</h3>
-          <p>TileNTaps is an e-commerce application that I built for my cousin’s business. The main functionality includes Authentication and Authorization using JSON Web Token, Error Handling with express.js, RESTful API's, implementation of the shopping cart, File Uploading, Sending Emails to customers, the app uses Amazon S3 to store static assets of the application. Stripe is used as a payment gateway. Application admin uses a user-friendly admin panel to check for new orders and to create new sanitary products.</p>
-          <br />
-          <div className="projectTechnologies">
-            <div>
-              <ul>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>icon</li>
-                <li>icon</li>
-              </ul>
-            </div>
-            <div>
-              <nav>
-                <span><a href="#"><GitHubIcon/></a></span>
-                <span><a href="#"><LaunchIcon/></a></span>
-              </nav>
-            </div>
-          </div>
-        </div>
-        <div className="projectDetail">
-          <h3>Title</h3>
-          <p>TileNTaps is an e-commerce application that I built for my cousin’s business. The main functionality includes Authentication and Authorization using JSON Web Token, Error Handling with express.js, RESTful API's, implementation of the shopping cart, File Uploading, Sending Emails to customers, the app uses Amazon S3 to store static assets of the application. Stripe is used as a payment gateway. Application admin uses a user-friendly admin panel to check for new orders and to create new sanitary products.</p>
-          <br />
-          <div className="projectTechnologies">
-            <div>
-              <ul>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>icon</li>
-                <li>icon</li>
-              </ul>
-            </div>
-            <div>
-              <nav>
-                <span><a href="#"><GitHubIcon/></a></span>
-                <span><a href="#"><LaunchIcon/></a></span>
-              </nav>
-            </div>
-          </div>
-        </div>
-        <div className="projectDetail">
-          <h3>Title</h3>
-          <p>TileNTaps is an e-commerce application that I built for my cousin’s business. The main functionality includes Authentication and Authorization using JSON Web Token, Error Handling with express.js, RESTful API's, implementation of the shopping cart, File Uploading, Sending Emails to customers, the app uses Amazon S3 to store static assets of the application. Stripe is used as a payment gateway. Application admin uses a user-friendly admin panel to check for new orders and to create new sanitary products.</p>
-          <br />
-          <div className="projectTechnologies">
-            <div>
-              <ul>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>icon</li>
-                <li>icon</li>
-              </ul>
-            </div>
-            <div>
-              <nav>
-                <span><a href="#"><GitHubIcon/></a></span>
-                <span><a href="#"><LaunchIcon/></a></span>
-              </nav>
-            </div>
-          </div>
-        </div>
-        <div className="projectDetail">
-          <h3>Title</h3>
-          <p>TileNTaps is an e-commerce application that I built for my cousin’s business. The main functionality includes Authentication and Authorization using JSON Web Token, Error Handling with express.js, RESTful API's, implementation of the shopping cart, File Uploading, Sending Emails to customers, the app uses Amazon S3 to store static assets of the application. Stripe is used as a payment gateway. Application admin uses a user-friendly admin panel to check for new orders and to create new sanitary products.</p>
-          <br />
-          <div className="projectTechnologies">
-            <div>
-              <ul>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>none</li>
-                <li>icon</li>
-                <li>icon</li>
-              </ul>
-            </div>
-            <div>
-              <nav>
-                <span><a href="#"><GitHubIcon/></a></span>
-                <span><a href="#"><LaunchIcon/></a></span>
-              </nav>
-            </div>
-          </div>
+          <Project />
         </div>
       </div>
     </>
